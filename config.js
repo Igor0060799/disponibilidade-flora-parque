5c8214203e52792c9398629d63873e8d57b4e520
Igor0060799 <igor.con.mar@gmail.com>
2025-03-02 21:39:45 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,25 @@
+<script type="module">
+  // Importe as funções necessárias do Firebase
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+  import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+
+  // Configuração do Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyBkg7c0CM6Ru7TgSTHO1rw-pYRaN222-Kg",
+    authDomain: "disponibilidade-flora-parque.firebaseapp.com",
+    projectId: "disponibilidade-flora-parque",
+    storageBucket: "disponibilidade-flora-parque.firebasestorage.app",
+    messagingSenderId: "69048295836",
+    appId: "1:69048295836:web:46a1c707dc05039c0d6859",
+    measurementId: "G-5MC5EGDMDH"
+  };
+
+  // Inicialize o Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const database = getDatabase(app); // Inicializa o Realtime Database
+</script>
 // Salvar coordenadas no Google Sheets
 saveButton.addEventListener('click', async () => {
   for (const dot of dots) {
